4f6226011a60e4455e420d865ef7d6d48cd48b0f
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-26 18:55:21 +0530

    Update tguploader.py

--- bot/core/tguploader.py
+++ bot/core/tguploader.py
@@ -57,11 +57,17 @@ class TgUploader:
             percent = round(current / total * 100, 2)
             speed = current / diff 
             eta = round((total - current) / speed)
-            bar = floor(percent/8)*"■" + (12 - floor(percent/8))*"□"
-            progress_str = f"""● <b>Status:</b> Uploading..
-● <code>[{bar}]</code> {percent}%   
-● <b>Size:</b> {convertBytes(current)} out of ~ {convertBytes(total)}
-● <b>Speed :</b> {convertBytes(speed)}/s
-● <b>ETA:</b> {convertTime(eta)}"""
+            bar = floor(percent/8)*"●" + (12 - floor(percent/8))*"○"
+            progress_str = f"""📌 <b>Anime Name :</b> <b><i>{self.__name}</i></b>
+
+🔄 <b>Status :</b> <i>Uploading 📤</i>
+    <code>[{bar}]</code> {percent}%
+    
+    📚 <b>Size :</b> {convertBytes(current)} out of ~ {convertBytes(total)}
+    🚀 <b>Speed :</b> {convertBytes(speed)}/s
+    ⏱ <b>Time Took :</b> {convertTime(diff)}
+    ⏳ <b>Time Left :</b> {convertTime(eta)}
+
+📂 <b>File(s) Encoded:</b> <code>{Var.QUALS.index(self.__qual)} / {len(Var.QUALS)}</code>"""
             
             await editMessage(self.message, progress_str)
